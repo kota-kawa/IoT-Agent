08595d9cc2722ef11f19949ac15eac01590e1880
kota-kawa <s2122126@cco.kanagawa-it.ac.jp>
2025-10-22 16:07:20 +0900

    fix

--- edge_device_code/raspberrypi-pico/secrets.py
+++ edge_device_code/raspberrypi-pico/secrets.py
@@ -1,7 +1,7 @@
 # Wi-Fi 認証情報（Pico W のフラッシュに保存）
 # 実運用では Git に載せないでください
-WIFI_SSID = "KAIT-WIFI"
-WIFI_PASSWORD = "a9cZfD5GR8v4"
+WIFI_SSID = "*****"
+WIFI_PASSWORD = "*****"
 
 # ダッシュボードに表示したい任意の情報（空のままでも可）
 DEVICE_LABEL = ""
