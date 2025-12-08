d9ae6622ae930ca4413efdd69949c35bdefb0826
kota-kawa <s2122126@cco.kanagawa-it.ac.jp>
2025-12-08 14:54:23 +0900

    Increase DEVICE_RESULT_TIMEOUT from 60s to 180s to prevent timeouts during sequential execution of multiple device commands

--- iot_agent/config.py
+++ iot_agent/config.py
@@ -14,6 +14,6 @@ AGENT_CAPABILITY_NAME = "agent_instruction"
 AGENT_COMMAND_NAME = "agent_instruction"
 
 MAX_COMPLETED_JOBS = int(os.getenv("MAX_COMPLETED_JOBS", "200"))
-DEVICE_RESULT_TIMEOUT = float(os.getenv("DEVICE_RESULT_TIMEOUT", "60"))
+DEVICE_RESULT_TIMEOUT = float(os.getenv("DEVICE_RESULT_TIMEOUT", "180"))
 
 SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "change-this-secret")
